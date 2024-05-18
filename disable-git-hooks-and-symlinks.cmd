7da02be73e4ce9bf5a11918ffc9db47d8c49cbda
geekgit <geeekz@gmail.com>
2024-05-18 00:54:50 +0000

    disable symlinks

--- disable-git-hooks-and-symlinks.cmd
+++ disable-git-hooks-and-symlinks.cmd
@@ -2,5 +2,7 @@
 echo Disable git hooks...
 echo Execute: "git config --global core.hooksPath c:\nul"
 git config --global core.hooksPath c:\nul
+echo Disable symlinks...
+git config --global core.symlinks false
 echo OK
 pause
